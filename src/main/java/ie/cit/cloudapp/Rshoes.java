c5865a969d992f130e2a448a9159cb1162097aab
Frank Crowe <fcrowe07@eircom.ne>
2012-05-09 22:29:18 +0100

    First Class - Rshoes

---
+++ src/main/java/ie/cit/cloudapp/Rshoes.java
@@ -0,0 +1,19 @@
+package ie.cit.cloudapp;
+
+public class Rshoes {
+
+	private Long id;
+	private String name;
+	private String title;
+
+	public void Runshoes() {
+	}
+
+	public Rshoes(Long id, String name, String title) {
+		this.id = id;
+		this.name = name;
+		this.title = title;
+	}
+
+	// Getters and setters omitted for brevity.
+}
